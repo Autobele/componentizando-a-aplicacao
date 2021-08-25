94cac9f5b878c15fd73d9db09021c49ae15b2770
Autobele <autobele@infoway-pi.com.br>
2021-08-24 23:25:16 -0300

    Refactoring components

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,24 @@
-export function Content() {
+import { MovieCard } from './MovieCard';
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+export function Content(props: any) {
   // Complete aqui
+  return (
+    <main>
+      <div className="movies-list">
+        {props.movies.map((movie: MovieProps) => (
+          <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+        ))}
+      </div>
+    </main>
+  )
 }
